d4995b7704fef1b8a9ec0f14ac0bfa136d5317c7
nuria <nurialeb207@gmail.com>
2020-03-12 12:20:29 +0100

    Base de datos

--- src/main/java/java/packDao/ConnectionManager.java
+++ src/main/java/java/packDao/ConnectionManager.java
@@ -1,22 +1,47 @@
 package java.packDao;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class ConnectionManager {
 
-	private static String url;
+	private static String bd;
 	private static String driverName;
 	private static String username;
 	private static String password;
 	private Connection connection;
 
 	public ConnectionManager() {
-		// TODO - implement ConnectionManager.ConnectionManager
+		Connection conexion = null;
+		try {
+			conexion = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + bd,username,password);
+			this.connection=conexion;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		throw new UnsupportedOperationException();
 	}
 
-	public Connection getConnection() {
+	public Connection getConnection() throws SQLException {
+
 		return this.connection;
 	}
 
+	public ResultSet execSQL(String sql) {
+		ResultSet res = null;
+		try {
+			Statement query = connection.createStatement();
+			if (sql.toLowerCase().contains("select")) {
+				res = query.executeQuery(sql);
+			}
+			else {
+				query.executeUpdate(sql);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println(e);
+			System.out.println("No se ha podido ejecutar la sql");
+		}
+		return res;
+	}
 }
