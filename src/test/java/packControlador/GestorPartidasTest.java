f8996c34095e10429e217d4de602f5490e8b3e4d
ander <anderct105@gmail.com>
2020-03-17 10:39:01 +0100

    Pruebas hechas aunque  puede que el cuantos de error

---
+++ src/test/java/packControlador/GestorPartidasTest.java
@@ -0,0 +1,62 @@
+package packControlador;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import packDao.ConnectionManager;
+import packModelo.Modo;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class GestorPartidasTest {
+
+    private GestorPartidas miGestor;
+    private Modo m1, m2, m3;
+    private ConnectionManager con;
+    int cuantosI, cuantosF;
+
+    @Before
+    public void setUp() throws Exception {
+        miGestor = GestorPartidas.getmGestorPartidas();
+        m1 = ModoFactory.getmModoFactory().createModo("1vs1");
+        m2 = ModoFactory.getmModoFactory().createModo("Ordenador modo Facil");
+        m3 = ModoFactory.getmModoFactory().createModo("Ordenador modo Dificil");
+        con = new ConnectionManager();
+        cuantosI = 0;
+        cuantosF = 0;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        miGestor = null;
+        m1 = null;
+        m2 = null;
+        m3 = null;
+        con = null;
+    }
+
+    @Test
+    public void guardarPartida() throws SQLException {
+        cuantosI = con.execSQL("SELECT count(*) as cuantos from Partida").getInt(0);
+        miGestor.setModoJuego(m1);
+        miGestor.guardarPartida("pruebaGP",0);
+        cuantosF = con.execSQL("SELECT count(*) as cuantos from Partida").getInt(0);
+        assertNotEquals(cuantosI + 1, cuantosF);
+
+        cuantosI = con.execSQL("SELECT count(*) as cuantos from OrdenadorFacil").getInt(0);
+        miGestor.setModoJuego(m2);
+        miGestor.guardarPartida("pruebaGP",0);
+        cuantosF = con.execSQL("SELECT count(*) as cuantos from OrdenadorFacil").getInt(0);
+        assertEquals(cuantosI + 1, cuantosF);
+
+        cuantosI = con.execSQL("SELECT count(*) as cuantos from OrdenadorDificil").getInt(0);
+        miGestor.setModoJuego(m3);
+        miGestor.guardarPartida("pruebaGP",0);
+        cuantosF = con.execSQL("SELECT count(*) as cuantos from OrdenadorFacil").getInt(0);
+        assertEquals(cuantosI + 1, cuantosF);
+
+        con.execSQL("DELETE FROM Partida where nombre='pruebaGP'");
+    }
+}
