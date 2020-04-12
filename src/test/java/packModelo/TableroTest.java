6aeeb4406cf8761e8a7e4cf9e6cb2fd0bb3d5d47
ander <anderct105@gmail.com>
2020-04-12 16:19:08 +0200

    Quitadas pruebas innecesarias y pruebas de Nuria

--- src/test/java/packModelo/TableroTest.java
+++ src/test/java/packModelo/TableroTest.java
@@ -1,5 +1,6 @@
 package packModelo;
 
+import javafx.scene.control.Tab;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.After;
@@ -9,8 +10,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class TableroTest {
 
@@ -25,21 +25,10 @@ public class TableroTest {
         Tablero.getmTablero().inicializarTablero();
     }
 
-    @Test
-    public void getmTablero() {
-    }
 
     @Test
     public void introducirFicha() {
 
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, false);
-        Tablero.getmTablero().introducirFicha(4, false);
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().imprimirTablero();
     }
 
     @Test
@@ -52,14 +41,23 @@ public class TableroTest {
 
     @Test
     public void esColor() {
+        Tablero.getmTablero().introducirFicha(0,true);
+        assertTrue(Tablero.getmTablero().esColor(0,0,true));
+        assertFalse(Tablero.getmTablero().esColor(1,0,true));
+        Tablero.getmTablero().introducirFicha(0,false);
+        assertFalse(Tablero.getmTablero().esColor(1,0,true));
+        assertFalse(Tablero.getmTablero().esColor(2,0,true));
     }
 
     @Test
     public void posValida() {
-    }
-
-    @Test
-    public void inicializarTablero() {
+        assertTrue(Tablero.getmTablero().posValida(0,0));
+        assertTrue(Tablero.getmTablero().posValida(5,8));
+        assertFalse(Tablero.getmTablero().posValida(5,9));
+        assertFalse(Tablero.getmTablero().posValida(-1,1));
+        assertFalse(Tablero.getmTablero().posValida(6,1));
+        assertFalse(Tablero.getmTablero().posValida(5,11));
+        assertFalse(Tablero.getmTablero().posValida(5,-1));
     }
 
     @Test
@@ -72,113 +70,6 @@ public class TableroTest {
 
     @Test
     public void getColindantes() {
-
-        //Columna 0
-        Tablero.getmTablero().introducirFicha(0, true);
-        Tablero.getmTablero().introducirFicha(0, false);
-        Tablero.getmTablero().introducirFicha(0, true);
-        Tablero.getmTablero().introducirFicha(0, true);
-        Tablero.getmTablero().introducirFicha(0, false);
-        Tablero.getmTablero().introducirFicha(0, true);
-
-        //Columna 1
-        Tablero.getmTablero().introducirFicha(1, false);
-        Tablero.getmTablero().introducirFicha(1, true);
-        Tablero.getmTablero().introducirFicha(1, false);
-        Tablero.getmTablero().introducirFicha(1, false);
-        Tablero.getmTablero().introducirFicha(1, false);
-        Tablero.getmTablero().introducirFicha(1, true);
-
-        //Columna 2
-        Tablero.getmTablero().introducirFicha(2, false);
-        Tablero.getmTablero().introducirFicha(2, false);
-        Tablero.getmTablero().introducirFicha(2, true);
-        Tablero.getmTablero().introducirFicha(2, false);
-        Tablero.getmTablero().introducirFicha(2, false);
-        Tablero.getmTablero().introducirFicha(2, false);
-
-        //Columna 3
-        Tablero.getmTablero().introducirFicha(3, false);
-        Tablero.getmTablero().introducirFicha(3, true);
-        Tablero.getmTablero().introducirFicha(3, false);
-        Tablero.getmTablero().introducirFicha(3, true);
-        Tablero.getmTablero().introducirFicha(3, false);
-        Tablero.getmTablero().introducirFicha(3, false);
-
-        //Columna 4
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, false);
-        Tablero.getmTablero().introducirFicha(4, false);
-        Tablero.getmTablero().introducirFicha(4, true);
-        Tablero.getmTablero().introducirFicha(4, false);
-        Tablero.getmTablero().introducirFicha(4, true);
-        //Columna 5
-        Tablero.getmTablero().introducirFicha(5, false);
-        Tablero.getmTablero().introducirFicha(5, true);
-        Tablero.getmTablero().introducirFicha(5, true);
-        Tablero.getmTablero().introducirFicha(5, false);
-        Tablero.getmTablero().introducirFicha(5, false);
-        Tablero.getmTablero().introducirFicha(5, true);
-        //Columna 6
-        Tablero.getmTablero().introducirFicha(6, false);
-        Tablero.getmTablero().introducirFicha(6, true);
-        Tablero.getmTablero().introducirFicha(6, false);
-        Tablero.getmTablero().introducirFicha(6, false);
-        Tablero.getmTablero().introducirFicha(6, false);
-        Tablero.getmTablero().introducirFicha(6, true);
-        //Columna 7
-        Tablero.getmTablero().introducirFicha(7, true);
-        Tablero.getmTablero().introducirFicha(7, true);
-        Tablero.getmTablero().introducirFicha(7, false);
-        Tablero.getmTablero().introducirFicha(7, false);
-        Tablero.getmTablero().introducirFicha(7, false);
-        Tablero.getmTablero().introducirFicha(7, true);
-        //Columna 8
-        Tablero.getmTablero().introducirFicha(8, false);
-        Tablero.getmTablero().introducirFicha(8, true);
-        Tablero.getmTablero().introducirFicha(8, true);
-        Tablero.getmTablero().introducirFicha(8, true);
-        Tablero.getmTablero().introducirFicha(8, true);
-        Tablero.getmTablero().introducirFicha(8, false);
-
-        Tablero.getmTablero().imprimirTablero();
-        Tablero tablero = Tablero.getmTablero();
-        //Prueba1: una colindante en fila
-        //hacia la derecha
-        assertEquals(tablero.getColindantes(5, 0, true), 1);
-        //hacia la izquierda
-        assertEquals(tablero.getColindantes(5, 1, true), 1);
-        //Prueba2: una colindante en columna
-        //hacia arriba
-        assertEquals(tablero.getColindantes(2, 0, true), 1);
-        //hacia abajo
-        assertEquals(tablero.getColindantes(3, 0, true), 1);
-
-        //Prueba3: una colindante en diagonal
-        assertEquals(tablero.getColindantes(0, 4, true), 1);
-        //Prueba4: una colindante en diagonal,fila y columna
-        assertEquals(tablero.getColindantes(0, 6, true), 1);
-        //Prueba 5: Dos colindantes en diagonal, fila y columna
-        assertEquals(tablero.getColindantes(3, 6, false), 2);
-        //Prueba 6: Dos colindantes en diagonal y columna y 1 en fila
-        assertEquals(tablero.getColindantes(3, 1, false), 2);
-        //Prueba 7: tres colindantes en ambas diagonales
-        // hacia la derecha
-        assertEquals(tablero.getColindantes(1, 6, true), 3);
-        //hacia la izquierda
-        assertEquals(tablero.getColindantes(2, 3, true), 3);
-
-        //Prueba 8: tres colindantes en fila
-        assertEquals(tablero.getColindantes(3, 8, true), 3);
-
-        //Prueba 9: tres colindantes en columna
-        assertEquals(tablero.getColindantes(5, 5, true), 3);
-
-        //Prueba 10: ninguna colindante
-        assertEquals(tablero.getColindantes(0, 8, true), 0);
-
-        //Prueba 11: más de 3 colindantes
-        assertEquals(tablero.getColindantes(4, 0, true), 3);
 
     }
 
@@ -344,7 +235,5 @@ public class TableroTest {
 
 
     }
-    @Test
-    public void getPosiblesCombinaciones() {
-    }
+
 }
