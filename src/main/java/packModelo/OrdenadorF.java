aa22b393263699057b8d1bb748c40cbf81c945e1
Naiara <naiaramaneiro07@gmail.com>
2020-04-13 15:54:38 +0200

    Método jugar() de OrdenadorF hecho

--- src/main/java/packModelo/OrdenadorF.java
+++ src/main/java/packModelo/OrdenadorF.java
@@ -1,27 +1,52 @@
 package packModelo;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.Random;
+
 public class OrdenadorF implements Modo {
+	private boolean turno;
 
-	public OrdenadorF() {
-		// TODO - implement OrdenadorF.OrdenadorF
-		throw new UnsupportedOperationException();
-	}
+	public OrdenadorF() {}
 
-	/**
-	 *
-	 * @param pColumna
-	 * @return
+	/**Pre: recibe como parámetro la columna introducida por el jugador
+	 * Post: devuelve un json que contiene la información de lo sucedido durante la partida
+	 * @author Naiara Maneiro
+	 * @param pColumna la columna en la que se va a introducir la ficha en la jugada
+	 * @return Json de la forma {x:int, y:int, lleno: boolean, posicionesGanadoras {x1:,y1:,x2:,y2:...}} que contiene la
+	 * información de lo sucedido durante la partida y nulo en caso de que no se haya podido introducir la ficha
 	 */
 	public JSONObject jugar(int pColumna) {
-		// TODO - implement OrdenadorF.jugar
-		throw new UnsupportedOperationException();
+		JSONObject json = Tablero.getmTablero().introducirFicha(pColumna,turno);
+		if(json != null){
+			Integer x = (Integer) json.get("x");
+			Integer y = (Integer) json.get("y");
+			JSONArray ganado = Tablero.getmTablero().haGanado(x,y,turno);
+			if (ganado == null){
+				turno =! turno;
+				boolean enc = false;
+				Random r = new Random();
+				while (!enc){
+					int col = r.nextInt(9);
+					boolean ocupada = Tablero.getmTablero().ocupada(col);
+					if (!ocupada){
+						Tablero.getmTablero().introducirFicha(col,turno);
+						enc = true;
+					}
+				}
+			} else{
+				json.put("posicionesGanadoras", ganado);
+			}
+			return json;
+		}
+		else{
+			return null;
+		}
 	}
 
 	public String getNombre() {
-		// TODO - implement OrdenadorF.getNombre
-		throw new UnsupportedOperationException();
+		return "OrdenadorF";
 	}
 
 }
