7e1c65e6582a112ed5486af1fd2735e0d995eb30
Igor <igor.game999.ig@gmai.com>
2020-04-19 16:42:03 +0200

    Pruebas de ordenador dificil hechas y funcionando

--- src/test/java/packModelo/OrdenadorDTest.java
+++ src/test/java/packModelo/OrdenadorDTest.java
@@ -5,6 +5,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Scanner;
+
 public class OrdenadorDTest extends TestCase {
 
     private Tablero t;
@@ -22,8 +24,36 @@ public class OrdenadorDTest extends TestCase {
     }
 
     public void testJugar() {
+        //EL JUGADOR VA A HACER CUATRO EN RAYA
         t.introducirFicha(0,true);
+        t.introducirFicha(1,true);
+        assertFalse(t.esColor(0,3,true));
+        assertFalse(t.esColor(0,3,false));
+        o.jugar(2);
+        assertTrue(t.esColor(0,3,false));
+
+        //EL ORDENADOR VA A HACER CUATRO EN RAYA
+        t.inicializarTablero();
+        t.introducirFicha(2, false);
+        t.introducirFicha(2, false);
+        t.introducirFicha(2, false);
+        assertFalse(t.esColor(3,2,true));
+        assertFalse(t.esColor(3,2,false));
         o.jugar(1);
-        t.imprimirTablero();
+        assertTrue(t.esColor(3,2,false));
+
+        //EL JUGADOR Y EL ORDENADOR VAN A HACER CUATRO EN RAYA
+        t.inicializarTablero();
+        t.introducirFicha(1, false);
+        t.introducirFicha(1, false);
+        t.introducirFicha(1, false);
+        t.introducirFicha(8, true);
+        t.introducirFicha(7, true);
+        assertFalse(t.esColor(3,1,true));
+        assertFalse(t.esColor(3,1,false));
+        o.jugar(6);
+        assertTrue(t.esColor(3,1,false));
+        assertFalse(t.esColor(0,5,true));
+        assertFalse(t.esColor(0,5,false));
     }
 }
