74bc0395efa8bf13b84d4ed0ae7813cc99346ed4
Igor <igor.game999.ig@gmai.com>
2020-04-19 14:21:35 +0200

    JUnit corregido

--- src/test/java/packModelo/OrdenadorDTest.java
+++ src/test/java/packModelo/OrdenadorDTest.java
@@ -1,5 +1,29 @@
+package packModelo;
+
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class OrdenadorDTest extends TestCase {
 
+    private Tablero t;
+    private OrdenadorD o;
+
+    public void setUp() throws Exception {
+        t = Tablero.getmTablero();
+        t.inicializarTablero();
+        o = new OrdenadorD();
+    }
+
+    public void tearDown() throws Exception {
+        t.inicializarTablero();
+        o = null;
+    }
+
+    public void testJugar() {
+        t.introducirFicha(0,true);
+        o.jugar(1);
+        t.imprimirTablero();
+    }
 }
