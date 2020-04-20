eb033a653c894be1eab27771c998802e2a5d939c
ander <anderct105@gmail.com>
2020-04-20 17:46:17 +0200

    Arreglado url ConnectionManager.java

--- src/main/java/packDao/ConnectionManager.java
+++ src/main/java/packDao/ConnectionManager.java
@@ -14,7 +14,7 @@ public class ConnectionManager {
 
 	public ConnectionManager() {
 		try {
-			Connection conexion = DriverManager.getConnection(driverName + "://" + server + ":" + port + bd,username,password);
+			Connection conexion = DriverManager.getConnection(driverName + "://" + server + ":" + port + "/ " + bd,username,password);
 			this.connection = conexion;
 		} catch (SQLException e) {
 			e.printStackTrace();
