5de2e8e39846a53dea8f31b0cbb80affd4dcb86b
ander <anderct105@gmail.com>
2020-04-21 14:40:23 +0200

    Probados varios fondos aunque ahora no hay ninguno establecido

--- src/main/java/packVista/IU_Menu.java
+++ src/main/java/packVista/IU_Menu.java
@@ -1,22 +1,33 @@
 package packVista;
 
-import javafx.animation.Animation;
-import javafx.animation.Interpolator;
-import javafx.animation.RotateTransition;
+import javafx.animation.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Duration;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import packControlador.Conecta4;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class IU_Menu {
+public class IU_Menu extends Stage {
 
     @FXML
     private TableView table_facil;
@@ -24,9 +35,18 @@ public class IU_Menu {
     private TableView table_dificil;
     @FXML
     private ImageView config;
+    @FXML
+    private AnchorPane botones;
+    @FXML
+    private ImageView play;
+    @FXML
+    private Label title;
+    @FXML
+    private AnchorPane pane;
 
     @FXML
     public void initialize() {
+        shakeStage();
         cargarRankingFacil();
     }
 
@@ -50,5 +70,43 @@ public class IU_Menu {
         rt.setCycleCount(1);
         rt.setInterpolator(Interpolator.LINEAR);
         rt.play();
+    }
+
+    @FXML
+    public void rotateBorder() {
+
+    }
+
+    @FXML
+    public void hoverPlay() {
+        ColorAdjust ca = new ColorAdjust();
+        ca.setBrightness(0.5);
+        play.setEffect(ca);
+    }
+
+    @FXML
+    public void quitHoverPlay() {
+        ColorAdjust ca = new ColorAdjust();
+        ca.setBrightness(0);
+        play.setEffect(ca);
+    }
+
+    @FXML
+    public void hoverConfig() {
+        ColorAdjust ca = new ColorAdjust();
+        ca.setBrightness(0.5);
+        config.setEffect(ca);
+    }
+
+    @FXML
+    public void quitHoverConfig() {
+        ColorAdjust ca = new ColorAdjust();
+        ca.setBrightness(0);
+        config.setEffect(ca);
+    }
+
+    public void shakeStage() {
+       ShakeTransition st = new ShakeTransition(title);
+       st.play();
     }
 }
