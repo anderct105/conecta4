be9a33a1e21e93be7da8ba576bcfeda7a339049d
Naiara <naiaramaneiro07@gmail.com>
2020-04-21 14:01:17 +0200

    Creado fxml de registrarPartida, hecha interfaz (queda dejarla bonita)

--- src/main/java/packVista/IU_RegistrarPartida.java
+++ src/main/java/packVista/IU_RegistrarPartida.java
@@ -1,4 +1,36 @@
 package packVista;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import packControlador.Conecta4;
+
+import java.awt.*;
+
 public class IU_RegistrarPartida {
+
+    @FXML
+    private TextField nombreU;
+    @FXML
+    private TextField puntuacionU;
+
+    @FXML
+    void pulsarGuardar(ActionEvent event){
+        registrarPartida();
+        //Cerrar la ventana actual y abrir el IU_Menu
+    }
+
+    @FXML
+    void volverInicio(ActionEvent event){
+        //Cerrar la ventana actual y abrir el IU_Menu
+    }
+
+    public void registrarPartida(){
+        String nombre = nombreU.getText();
+        int puntuacion = Integer.parseInt(puntuacionU.getText());
+        Conecta4.getmConecta4().guardarPartida(nombre, puntuacion);
+    }
 }
