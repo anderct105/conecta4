6965968b02d1a72451daf2d4e20d8d9b694de045
Naiara <naiaramaneiro07@gmail.com>
2020-04-21 15:31:02 +0200

    Funcionalidades de la interfaz hechas

--- src/main/java/packVista/IU_RegistrarPartida.java
+++ src/main/java/packVista/IU_RegistrarPartida.java
@@ -6,9 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import packControlador.Conecta4;
-
-import java.awt.*;
+import java.io.IOException;
 
 public class IU_RegistrarPartida {
 
@@ -20,12 +20,14 @@ public class IU_RegistrarPartida {
     @FXML
     void pulsarGuardar(ActionEvent event){
         registrarPartida();
-        //Cerrar la ventana actual y abrir el IU_Menu
+        cerrarVentana();
+        abrirVentana();
     }
 
     @FXML
     void volverInicio(ActionEvent event){
-        //Cerrar la ventana actual y abrir el IU_Menu
+        cerrarVentana();
+        abrirVentana();
     }
 
     public void registrarPartida(){
@@ -33,4 +35,25 @@ public class IU_RegistrarPartida {
         int puntuacion = Integer.parseInt(puntuacionU.getText());
         Conecta4.getmConecta4().guardarPartida(nombre, puntuacion);
     }
+
+    //Cerrar ventana JavaFX
+    public void cerrarVentana(){
+        Stage stage = (Stage) nombreU.getScene().getWindow();
+        stage.close();
+    }
+
+    //Abrir IU_Menu JavaFX
+    public void abrirVentana() {
+        Stage primaryStage = new Stage();
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        primaryStage.setTitle("Conecta 4");
+        primaryStage.setScene(new Scene(root, 1100, 600));
+        primaryStage.show();
+    }
+
 }
