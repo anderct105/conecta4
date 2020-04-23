c5dfd293887d0d9f2e665807d00ab21d13734863
ander <anderct105@gmail.com>
2020-04-23 16:42:14 +0200

    Puesto fondo transparente y tilde en la letra

--- src/main/java/packMain/Main.java
+++ src/main/java/packMain/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -16,8 +17,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/ColumnaLlena.fxml"));
-        primaryStage.setScene(new Scene(root, 400, 100));
+        Scene s = new Scene(root, 291, 100);
+        primaryStage.setScene(s);
         primaryStage.initStyle(StageStyle.UNDECORATED);
+        s.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(s);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.show();
     }
 }
--- src/main/java/packVista/IU_ColumnaLlena.java
+++ src/main/java/packVista/IU_ColumnaLlena.java
@@ -1,4 +1,33 @@
 package packVista;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class IU_ColumnaLlena {
+
+    @FXML
+    private Button aceptar;
+    @FXML
+    private Label mensaje;
+
+    /*  El siguiente código es necesario para la transparencia
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        s.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(s);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+     */
+
+    @FXML
+    public void initialize() {
+        mensaje.setText("Esta columna está llena. Elige otra.");
+    }
+
+    @FXML
+    public void cerrar(){
+        Stage stage = (Stage) aceptar.getScene().getWindow();
+        stage.close();
+    }
+
 }
