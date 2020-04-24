dc214f091818f6beb06e5ea66e7ca68098eaf239
Alvaro <universidad880350@gmail.com>
2020-04-24 12:32:15 +0200

    Inicializado

--- src/main/java/packVista/IU_Configuracion.java
+++ src/main/java/packVista/IU_Configuracion.java
@@ -1,4 +1,25 @@
 package packVista;
 
-public class IU_Configuracion {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class IU_Configuracion extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../animations.fxml"));
+        primaryStage.setTitle("Conecta 4");
+        primaryStage.setScene(new Scene(root, 400, 200));
+        primaryStage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
