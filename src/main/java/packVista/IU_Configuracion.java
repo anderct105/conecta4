69a880e6c87cd34c65338a1047c2d0830db8acb4
Alvaro <universidad880350@gmail.com>
2020-04-24 15:52:30 +0200

    Configuracion v1

--- src/main/java/packVista/IU_Configuracion.java
+++ src/main/java/packVista/IU_Configuracion.java
@@ -1,25 +1,46 @@
 package packVista;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import packControlador.Conecta4;
 
-import static javafx.application.Application.launch;
 
-public class IU_Configuracion extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../animations.fxml"));
-        primaryStage.setTitle("Conecta 4");
-        primaryStage.setScene(new Scene(root, 400, 200));
-        primaryStage.show();
+public class IU_Configuracion {
+
+    @FXML
+    private Button guardar;
+    @FXML
+    private Button cerrar;
+    @FXML
+    private ComboBox modoJuego;
+
+
+    @FXML
+    public void initialize(){
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "1vs1",
+                        "Ordenador modo Facil",
+                        "Ordenador modo Dificil"
+                );
+        modoJuego = new ComboBox(options);
+    }
+
+    @FXML
+    public void guardar(){
+        String modoElegido = (String) modoJuego.getValue();
+        Conecta4.getmConecta4().setModoJuego(modoElegido);
+    }
+
+    @FXML
+    public void cerrar(){
+        Stage stage = (Stage) cerrar.getScene().getWindow();
+        stage.close();
     }
 
 
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
