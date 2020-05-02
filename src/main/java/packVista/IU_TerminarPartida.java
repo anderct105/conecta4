fad6f9ab86ef3ba9d7a7cf73e062d263f62e285d
nuria <nurialeb207@gmail.com>
2020-05-02 23:23:38 +0200

    Puntuación derrota eliminada y ayuda

--- src/main/java/packVista/IU_TerminarPartida.java
+++ src/main/java/packVista/IU_TerminarPartida.java
@@ -86,25 +86,39 @@ public class IU_TerminarPartida {
     }
 
     public void inicializar(int tiempo, int resultado) {
-        this.tiempo = tiempo;
+        if(resultado==1){
+            this.tiempo = tiempo;
+        }
+        else{
+            this.tiempo = 0;
+           guardar.setVisible(false);
+        }
+
         this.resultado = resultado;
         ponerPuntuacion();
         ponerImagen();
     }
 
     public void ponerPuntuacion() {
-        puntuacion.setText("La puntuacion obtenida es: " + tiempo);
+      puntuacion.setText("La puntuacion obtenida es: " + tiempo);
+
     }
 
     public void ponerImagen() {
         Image image;
         switch (resultado) {
+
             case 0:
-                if (Conecta4.getmConecta4().getModoJuego().equals("1vs1"))
+                if (Conecta4.getmConecta4().getModoJuego().equals("1vs1")) {
                     image = new Image("/imagenes/ganadoAzul.gif",
                             415, 275, false, false);
-                else image = new Image("/imagenes/derrota.jpg",
-                        415, 275, false, false);
+                    puntuacion.setVisible(false);
+                    guardar.setVisible(false);
+                }
+                else {
+                    image = new Image("/imagenes/derrota.jpg",
+                            415, 275, false, false);
+                }
                 break;
             case 1:
                 image = new Image("/imagenes/ganadoRojo.gif",
